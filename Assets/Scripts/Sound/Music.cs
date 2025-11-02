bffeeb2559984225bcf865c7e9a67884bcb52d26
Cortez25 <andrey.harin@list.ru>
2025-11-02 19:11:36 +0300

    Fmod music script

---
+++ Assets/Scripts/Sound/Music.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class BandPerformanceManager : MonoBehaviour
+{
+    [Header("FMOD Events")]
+    [Tooltip("Speaking")]
+    public List<EventReference> speakingEvents;
+
+    [Tooltip("Music")]
+    public List<EventReference> musicEvents;
+
+    private EventInstance currentEvent;
+
+    void Start()
+    {
+        StartCoroutine(PerformanceLoop());
+    }
+
+    private IEnumerator PerformanceLoop()
+    {
+        while (true)
+        {
+            // --- SPEAKING PART ---
+            if (speakingEvents.Count > 0)
+            {
+                var speakingEvent = speakingEvents[Random.Range(0, speakingEvents.Count)];
+                yield return PlayAndWaitForEvent(speakingEvent);
+            }
+
+            // --- MUSIC PART ---
+            if (musicEvents.Count > 0)
+            {
+                var musicEvent = musicEvents[Random.Range(0, musicEvents.Count)];
+                yield return PlayAndWaitForEvent(musicEvent);
+            }
+        }
+    }
+
+    private IEnumerator PlayAndWaitForEvent(EventReference eventRef)
+    {
+        currentEvent = RuntimeManager.CreateInstance(eventRef);
+
+        // ✅ Set 3D attributes to the GameObject's transform
+        currentEvent.set3DAttributes(RuntimeUtils.To3DAttributes(transform));
+
+        currentEvent.start();
+
+        // Wait until event stops
+        bool isPlaying = true;
+        while (isPlaying)
+        {
+            currentEvent.getPlaybackState(out PLAYBACK_STATE state);
+            if (state == PLAYBACK_STATE.STOPPED || state == PLAYBACK_STATE.STOPPING)
+            {
+                isPlaying = false;
+            }
+            yield return null;
+        }
+
+        currentEvent.release();
+    }
+}
